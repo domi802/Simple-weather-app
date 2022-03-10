54a0ee6e86bf2d9b850c39b14c89f3041a13590a
domi802 <64065747+domi802@users.noreply.github.com>
2022-03-10 16:59:37 +0100

    Initial Commit

---
+++ GoodWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodWeather
+//
+//  Created by Dominik Woźniak on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodWeather/GoodWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherApp.swift
+//  GoodWeather
+//
+//  Created by Dominik Woźniak on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
